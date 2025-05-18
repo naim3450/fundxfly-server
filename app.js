86bbb975a2976b0bc9523cc4ba95522927c68351
naim3450 <naim.saim74@gmail.com>
2025-05-18 15:31:58 +0600

    change url

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin: [
         // "http://localhost:3001",
         "https://fundxfly.com",
-        "https://admin.fundxfly.com",
+        "http://dashboard.fundxfly.com",
         "https://testpart.fundxfly.com",
         // "http://localhost:5173",
         // "http://localhost:5174",
